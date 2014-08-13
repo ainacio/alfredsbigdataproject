8272f2660a2d48b1238ff27c3094d1807918eca2
ainacio <ainacio@192.168.0.10>
2014-08-13 11:09:39 -0400

    message2

--- Big Data Project/src/big/data/project/BigDataProject.java
+++ Big Data Project/src/big/data/project/BigDataProject.java
@@ -18,6 +18,7 @@ public class BigDataProject {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("test2");
+        System.out.println("test3");
     }
     
 }
